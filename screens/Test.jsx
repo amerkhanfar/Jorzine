e548878a0a144096ad104c443a9a4ab7de972159
amerkhanfar <abk.sg8@gmail.com>
2023-05-04 17:27:12 +0400

    created the footer section

---
+++ screens/Test.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const Test = () => {
+  return <Container>Test</Container>;
+};
+
+export default Test;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-image: url("/assets/bottom.png");
+  background-size: cover;
+
+  background: white;
+`;
