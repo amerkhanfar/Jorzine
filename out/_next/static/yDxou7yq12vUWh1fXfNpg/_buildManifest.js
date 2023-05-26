5d09cb480d3fa2d613ca5e82698ac74f5a03da14
amerkhanfar <abk.sg8@gmail.com>
2023-05-26 14:03:59 +0400

    added out folder

---
+++ out/_next/static/yDxou7yq12vUWh1fXfNpg/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-9368eb543d116c97.js","static/chunks/936-b637c7a3338f783e.js","static/chunks/pages/index-1b8a4e078a47cb54.js"],"/_error":["static/chunks/pages/_error-afb6041758c6489b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
