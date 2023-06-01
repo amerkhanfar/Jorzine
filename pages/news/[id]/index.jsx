d692442c44561486854f0576c460ace2acc6cf5c
amerkhanfar <abk.sg8@gmail.com>
2023-06-01 12:16:08 +0400

    removed getstaticpath

--- pages/news/[id]/index.jsx
+++ pages/news/[id]/index.jsx
@@ -91,24 +91,24 @@ const Index = () => {
 
 export default Index;
 
-export async function getStaticProps() {
-  return {
-    props: { title: "title", description: "description" },
-  };
-}
+// export async function getStaticProps() {
+//   return {
+//     props: { title: "title", description: "description" },
+//   };
+// }
 
-export async function getStaticPaths() {
-  const ids = [17, 20, 21]; // Example array of IDs
+// export async function getStaticPaths() {
+//   const ids = [17, 20, 21]; // Example array of IDs
 
-  // Map the IDs to the `params` object required by Next.js
-  const paths = ids.map((id) => ({
-    params: { id: id.toString() },
-  }));
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//   // Map the IDs to the `params` object required by Next.js
+//   const paths = ids.map((id) => ({
+//     params: { id: id.toString() },
+//   }));
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
 const Container = styled.div`
   display: flex;
