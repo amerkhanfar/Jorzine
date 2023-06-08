48bcdb0fb6d5c3ee6ed2496c33288b603df160af
amerkhanfar <abk.sg8@gmail.com>
2023-06-08 13:53:25 +0400

    changed status bar color

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ export default function Home() {
         />
         <link rel='manifest' href='/site.webmanifest' />
         <meta name='msapplication-TileColor' content='#da532c' />
-        <meta name='theme-color' content='#ffffff'></meta>
+        <meta name='theme-color' content='#000'></meta>
       </Head>
       {side ? (
         <Side>
