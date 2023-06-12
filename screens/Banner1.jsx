9a0127d2c067cefc0d80c3913ecc8f01fad440fd
amerkhanfar <abk.sg8@gmail.com>
2023-06-12 16:07:54 +0400

    fixed spotify widget

--- screens/Banner1.jsx
+++ screens/Banner1.jsx
@@ -286,11 +286,15 @@ const Banners = () => {
         </BannerText3> */}
 
         <Mcolumn>
-          <Playlist
+          <iframe
+            style={{ borderRadius: "14px" }}
             src='https://open.spotify.com/embed/playlist/6GIfItyTdvjxKVVxkqfVpc?utm_source=generator&theme=0'
+            width='100%'
+            height='152'
+            frameBorder='0'
             allowfullscreen=''
             allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture'
-            loading='lazy'></Playlist>
+            loading='lazy'></iframe>
           <h1 style={{ color: "white" }}>Latest Reviews</h1>
           {reviews.map((item) => {
             return (
